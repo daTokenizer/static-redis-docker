a9c772eb0d1cd9c6f6b07e5d2a992b9a13e38ef6
Adam Lev-Libfeld <adam.inf@gmail.com>
2019-03-17 17:34:19 +0200

    enforce tags to be lower case

--- run.py
+++ run.py
@@ -22,7 +22,7 @@ def main(args):
             data_date = os.path.getmtime(rdb_file)
             data_version = datetime.fromtimestamp(data_date).strftime("%Y%m%d-%H")
 
-        filename = os.path.basename(rdb_file).split(".")[0]
+        filename = os.path.basename(rdb_file).split(".")[0].lower()
         docker_build_command = f"sudo docker build --build-arg data_file={rdb_file} -t mvdb_{filename}:{data_version} ."
         print(f"building docker image using command: {docker_build_command}")
         os.system(docker_build_command)
